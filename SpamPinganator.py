366de3925b51e18f0d2d425f91e56de018084671
Jason Barrow <135932616+DynamiteBog@users.noreply.github.com>
2026-08-31 09:15:18 -0500

    Enhance SpamPinginator with user-defined spam input
    Added user input for spam message and count, with a delay between messages.

--- SpamPinganator.py
+++ SpamPinganator.py
@@ -2,12 +2,17 @@ import pyautogui
 import time
 
 def SpamPinginator():
-    x = 0
-    y = 100
-    z = 1
+    ping = input("What should I spam: ")
+    spam = int(input("How many times: "))
+
+    print("You have 5 seconds to click into the window...")
     time.sleep(5)
-    for i in range(x, y, z):
-        pyautogui.typewrite("@ChudGPT")
+
+    for i in range(spam):
+        pyautogui.typewrite(ping)
+        time.sleep(0.1)
         pyautogui.press("enter")
 
 SpamPinginator()
+
+#added functionality to allow user to say how many times they want to spam the victim, although discord does get mad after a certain point
